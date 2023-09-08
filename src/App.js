0e2629eb18798a2d6fc201de7f7d342dfc28faaa
David Custodio <jose.custodio@udgvirtual.udg.mx>
2023-09-08 00:37:50 -0600

    primer compromiso

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./img/logo.png";
+import './App.css';
+
+
+function App() {
+
+    return (
+      <header className="App">
+        <div className="inicio">
+        <img src={logo} alt="logo" />
+        </div>
+       <h1> salud y belleza </h1>
+      </header>   
+      
+  );
+}
+
+export default App;
