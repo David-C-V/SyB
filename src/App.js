48dd56ce7317061d0ba1f86b43c163359b145496
David Custodio <jose.custodio@udgvirtual.udg.mx>
2023-09-25 22:37:57 -0600

    actualizacion

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import './App.css';
 
 
 function App() {
-
     return (
-      <header className="App">
-        <div className="inicio">
-        <img src={logo} alt="logo" />
+      <div className="inicio">
+        
+        <img src={logo} alt="logo" id="logo" />
+        <h1>Salud y Belleza</h1>
+        <h2>
+        Esta compañía está enfocada en apoyar a tu salud, 
+        ya que contamos con varios tratamientos como: planes 
+        de alimentación personalizados, mesoterapia, madero 
+        terapia, acupuntura, copas colombianas, masajes reductivos 
+        con aparatología, entre otros servicios los cuales los podrás 
+        ver en el menú de servicio.</h2>
+        <h2>
+        También contamos con una gran variedad de productos de 
+        belleza para tu cabello: como el óleo capilar, crema nutritiva, 
+        shampo…. para tu cara: cremas nutritivas, lápiz labial, sombra, 
+        rimen... para tu cuerpo: cremas hidratantes, cremas exfoliantes, 
+        desodorantes, perfumes, protector solar… todos esto productos y 
+        más los puedes ver en el menú de Productos. 
+        </h2>
         </div>
-       <h1> salud y belleza </h1>
-      </header>   
-      
   );
+
 }
+
 
 export default App;
